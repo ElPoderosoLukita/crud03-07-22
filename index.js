3b2b17458fbd68a31746d46d34d320cf4af2710f
Lucas Da Rosa <lucasdarosa5580@gmail.com>
2022-07-03 10:35:31 -0300

    Utilizamos las rutas en index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const router = require('./routes/index');
+
 const app = express();
+
+router(app);
 
 app.listen(3000, () => {
     console.log("[SERVER] initializated on port 3000");
