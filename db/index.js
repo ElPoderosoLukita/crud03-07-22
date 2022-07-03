06fabed677f44d165153f1fbffa320b619bace01
Lucas Da Rosa <lucasdarosa5580@gmail.com>
2022-07-03 17:52:04 -0300

    Modificaciones, añadimos que base de datos utilizaremos

--- db/index.js
+++ db/index.js
@@ -5,6 +5,7 @@ const con = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 con.connect((err) => {
