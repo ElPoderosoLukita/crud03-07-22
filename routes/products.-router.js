e76d61c47aba12a894eb83bdef2417ddc4e72004
Lucas Da Rosa <lucasdarosa5580@gmail.com>
2022-07-03 17:52:56 -0300

    Establecemos las rutas para cada uno de los servicios creados

--- routes/products.-router.js
+++ routes/products.-router.js
@@ -1,24 +1,139 @@
 const express = require('express');
+const ProductServices = require('../services/product-services');
+
 const router = express.Router();
+const productServices = new ProductServices();
+
+router.use(express.json());
+router.use(express.urlencoded({extended: false}));
 
 router.get('/', (req, res) => {
-    res.send('get product');
-})
+    try{
+    productServices.get((results) => {
+        res.status(200).json(results);
+    })
+    } catch (err){
+        res.json({
+            error: true,
+            message: error.message
+        });
+    }
+});
+
 
 router.get('/:id', (req, res) => {
-    res.send('get product by id');
-})
+    const id = req.params.id;
+    try{
+        productServices.getByID(id, (result) => {
+            res.status(200).json(result);
+        })
+    } catch (err) {
+        res.json({
+            error: true,
+            message: error.message
+        });
+    }
+});
+
 
 router.post('/', (req, res) => {
-    res.send('post product');
-})
+    const body = req.body;
+    try{
+        productServices.post(body)
+        res.status(201).json({
+            "status":201,
+            "message":"El producto ha sido creado correctamente!"
+        });
+    } catch (err){
+        res.json({
+            error: true,
+            message: err.message
+        });
+    }
+});
 
 router.put('/', (req, res) => {
-    res.send('put product');
-})
+    const body = req.body;
+    try{
+        productServices.put(body);
+        res.status(200).json({
+            "status":200,
+            "message":"El producto ha sido actualizado correctamente!"
+        });
+    } catch (err){
+        res.json({
+            error: true,
+            message: err.message
+        });
+    }
+});
 
 router.delete('/', (req, res) => {
-    res.send('delete product');
+    const body = req.body;
+
+    try{
+        productServices.delete(body);
+        res.status(200).json({
+            "status":200,
+            "message":"El producto ha sido eliminado correctamente!"
+        });
+    } catch (err){
+        res.json({
+            error: true,
+            message: err.message
+        });
+    }
+});
+
+router.patch('/nombre', (req, res) => {
+    const body = req.body;
+
+    try{
+        productServices.patchNombre(body);
+        res.status(200).json({
+            "status":200,
+            "message":"El nombre del producto ha sido actualizado correctamente!"
+        });
+    } catch (err){
+        res.json({
+            error: true,
+            message: err.message
+        });
+    }
+});
+
+router.patch('/precio', (req, res) => {
+    const body = req.body;
+
+    try{
+        productServices.patchPrecio(body);
+        res.status(200).json({
+            "status":200,
+            "message":"El precio del producto ha sido actualizado correctamente!"
+        });
+    } catch (err){
+        res.json({
+            error: true,
+            message: err.message
+        });
+    }
+})
+
+router.patch('/vendedor', (req, res) => {
+    const body = req.body;
+
+    try{
+        productServices.patchVendedor(body);
+        res.status(200).json({
+            "status":200,
+            "message":"El vendedor del producto ha sido actualizado correctamente!"
+        });
+    } catch (err){
+        res.json({
+            error: true,
+            message: err.message
+        });
+    }
 })
 
 module.exports = router;
